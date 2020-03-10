29c74b77c1e93d78859b5d5ff14c4ef32cc8606b
Erickson Zhuang <bingluen0224@gmail.com>
2020-03-10 20:55:23 +0800

    Add field for token, actid and invoke birth yyy

--- js/tax/msk.apply.js
+++ js/tax/msk.apply.js
@@ -248,14 +248,14 @@
         );
     }
     function send() {
-        msk.data.dataU = { 'hash': window.API_HASH };
+        msk.data.dataU = { 'ActID': window.API_ACTID, 'token': window.API_TOKEN };
     	msk.data.dataU.idn = $("#applyModal_msk001_idn").val();
     	msk.data.dataU.nm = $("#applyModal_msk001_nm").val();
     	msk.data.dataU.marketCd = $("#applyModal_msk001_storeCd").val().substring(0, 2);
     	msk.data.dataU.storeCd = $("#applyModal_msk001_storeCd").val().substring(2);
     	msk.data.dataU.birth = $("#applyModal_msk001_birth").val();
     	msk.data.dataU.mobileNumber = $("#applyModal_msk001_mobileNumber").val();
-    	msk.data.dataU.email = $("#applyModal_msk001_email").val();
+        msk.data.dataU.email = $("#applyModal_msk001_email").val();
         msk.send(window.API_SERVER + "/uploadService.do", JSON.stringify(msk.data.dataU), function (data) {
                 if (data.code < 0) {
                     msk.dialog.bootBox('' + data.message);
